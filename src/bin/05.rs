e589c03e50d8645fac68c642db7fff65788fa2d1
Mathieu Pagé <m@mathieupage.com>
2025-12-09 07:47:03 -0500

    End of day 5 part 1

---
+++ src/bin/05.rs
@@ -0,0 +1,66 @@
+#[derive(Debug)]
+struct Range {
+    start: u64,
+    end: u64,
+}
+
+#[derive(Debug)]
+struct Data {
+    ranges: Vec<Range>,
+    ingredients: Vec<u64>,
+}
+
+fn parse_input(input: &str) -> Data {
+    let lines: Vec<&str> = input.lines().collect();
+    let mut index = 0;
+    let mut ranges = Vec::new();
+    while !lines[index].is_empty() {
+        let (start_str, end_str) = lines[index].split_once('-').unwrap();
+        ranges.push(Range {
+            start: start_str.parse().unwrap(),
+            end: end_str.parse().unwrap(),
+        });
+        index += 1;
+    }
+
+    index += 1; // Skip the empty line
+
+    let ingredients: Vec<u64> = lines[index..]
+        .iter()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    Data {
+        ranges: ranges,
+        ingredients: ingredients,
+    }
+}
+
+fn part1(data: Data) -> u64 {
+    data.ingredients
+        .iter()
+        .filter(|ingredient| {
+            data.ranges
+                .iter()
+                .any(|range| range.start <= **ingredient && **ingredient <= range.end)
+        })
+        .count() as u64
+}
+
+fn main() {
+    let input = include_str!("../../inputs/05.txt");
+    let data = parse_input(input);
+    println!("Part 1: {}", part1(data));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_part1() {
+        let input = include_str!("../../examples/05.txt");
+        let data = parse_input(input);
+        assert_eq!(part1(data), 3);
+    }
+}
